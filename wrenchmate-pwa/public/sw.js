019c217ddf16d8acfc1b353c990381145cb7378a
96mph <duracak18@gmail.com>
2025-08-13 15:25:53 -0400

    Add files via upload

---
+++ wrenchmate-pwa/public/sw.js
@@ -0,0 +1,15 @@
+// Minimal service worker for offline shell caching
+const CACHE_NAME = 'wrenchmate-cache-v1';
+const URLS_TO_CACHE = ['/', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
